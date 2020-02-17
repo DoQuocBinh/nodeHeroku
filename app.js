fc3ad685a75a843f99ecc1911cb2d290608df2f7
Binh Do Quoc <binhdq@fpt.edu.vn>
2020-02-17 15:54:37 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var Addition=require('./Addition.js');
+function Multiple(a,b){
+    return a*b;
+}
+console.log(Addition.AddNumber(10,22));
+console.log(Addition.Substract(20,2));
+console.log(Multiple(4,5));
